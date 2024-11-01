0c5dc19a0e00ca318f1320171405a458eb75994f
Yaroslav Morgun <yaroslavmorgun@MacBook-Air-Yaroslav.local>
2024-11-01 17:51:12 +0200

    upload lab5

--- lab2/deps/js/burger.js
+++
@@ -1,7 +0,0 @@
-const burger = document.getElementById('burger');
-const mobileMenu = document.getElementById('mobileMenu');
-
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    mobileMenu.classList.toggle('active');
-});
